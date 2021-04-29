002a89818294a583288f6b1c17f3ba5f0f2c8259
Matt Beaudoin <ehxtwo@gmail.com>
2021-04-28 17:00:27 -0700

    commit tree 436999c9ea31a2869961da79c2f62e9230875c81

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,16 +12,21 @@ class SongsController < ApplicationController
 	end
 
 	def create
+		binding.pry
+		# artist = Artist.find_or_create_by(name: (name: params[:song][:artist])
+		# genre = Genre.find_or_create_by(name: params[:song][:genre])
 		@song = Song.new(params.require(:song).permit(:name))
+		@song.artist = Artist.find_or_create_by(name: params[:song][:artist])
+		@song.genre = Genre.find_or_create_by(name: params[:song][:genre])
 		@song.save
 		redirect_to song_path(@song)
-	  end
-	  
-	  def update
+	end
+	
+	def update
 		@song = Song.find(params[:id])
 		@song.update(params.require(:song).permit(:name))
 		redirect_to song_path(@song)
-	  end
+	end
 
 	def edit
 	  @song = Song.find(params[:id])
